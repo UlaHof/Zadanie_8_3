f3540566a4c6c695e91b2a64f083105532d2408c
Ula <chcetylkoobejrzecfilm@gmail.com>
2017-11-28 02:14:26 +0100

    Updated gruntfile.js and package.json, added GRUNT

--- gruntfile.js
+++ gruntfile.js
@@ -11,10 +11,14 @@ module.exports = function(grunt) {
                 'css/style.css': 'css/style.scss'
               }
             }
-          }
+          },
+            jshint: {
+              all: ['js/*.js']
+            }
       });
       // Load the plugins tasks
       grunt.loadNpmTasks('grunt-sass')
+      grunt.loadNpmTasks('grunt-contrib-jshint');
       // Default task(s).
-      grunt.registerTask('default', ['sass']);
+      grunt.registerTask('default', ['sass'],['jshint']);
     };
